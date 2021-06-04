05d6c53832b8f0dd8f6ccfdbd0bf6756789a0421
casia casia <casia16@steve>
2021-06-04 15:09:04 +0200

    samples_downsampling

---
+++ evaluation/samples_downsampling/runSATC.sh
@@ -0,0 +1,106 @@
+OUT_FOLD="/home/casia16/SATCnew/review"
+  
+  
+  nice Rscript --vanilla ./satc.R leopard_prophet10 ${OUT_FOLD}/leopard_prophet10.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R leopard_prophet25 ${OUT_FOLD}/leopard_prophet25.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R leopard_prophet50 ${OUT_FOLD}/leopard_prophet50.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R leopard_prophet75 ${OUT_FOLD}/leopard_prophet75.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R leopard_prophom10 ${OUT_FOLD}/leopard_prophom10.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R leopard_prophom25 ${OUT_FOLD}/leopard_prophom25.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R leopard_prophom50 ${OUT_FOLD}/leopard_prophom50.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R leopard_prophom75 ${OUT_FOLD}/leopard_prophom75.txt 
+${OUT_FOLD} &
+    
+  nice Rscript --vanilla ./satc.R impala_prophet10 ${OUT_FOLD}/impala_prophet10.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R impala_prophet25 ${OUT_FOLD}/impala_prophet25.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R impala_prophet50 ${OUT_FOLD}/impala_prophet50.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R impala_prophet75 ${OUT_FOLD}/impala_prophet75.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R impala_prophom10 ${OUT_FOLD}/impala_prophom10.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R impala_prophom25 ${OUT_FOLD}/impala_prophom25.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R impala_prophom50 ${OUT_FOLD}/impala_prophom50.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R impala_prophom75 ${OUT_FOLD}/impala_prophom75.txt 
+${OUT_FOLD} &
+    
+  nice Rscript --vanilla ./satc.R finches_prophet10 ${OUT_FOLD}/finches_prophet10.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R finches_prophet25 ${OUT_FOLD}/finches_prophet25.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R finches_prophet50 ${OUT_FOLD}/finches_prophet50.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R finches_prophet75 ${OUT_FOLD}/finches_prophet75.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R finches_prophom10 ${OUT_FOLD}/finches_prophom10.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R finches_prophom25 ${OUT_FOLD}/finches_prophom25.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R finches_prophom50 ${OUT_FOLD}/finches_prophom50.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R finches_prophom75 ${OUT_FOLD}/finches_prophom75.txt 
+${OUT_FOLD} &
+    
+  
+  nice Rscript --vanilla ./satc.R muskox_prophet10 ${OUT_FOLD}/muskox_prophet10.txt 
+${OUT_FOLD} & ## ??
+    nice Rscript --vanilla ./satc.R muskox_prophet25 ${OUT_FOLD}/muskox_prophet25.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R muskox_prophet50 ${OUT_FOLD}/muskox_prophet50.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R muskox_prophet75 ${OUT_FOLD}/muskox_prophet75.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R muskox_prophom10 ${OUT_FOLD}/muskox_prophom10.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R muskox_prophom25 ${OUT_FOLD}/muskox_prophom25.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R muskox_prophom50 ${OUT_FOLD}/muskox_prophom50.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R muskox_prophom75 ${OUT_FOLD}/muskox_prophom75.txt 
+${OUT_FOLD} &
+  
+  nice Rscript --vanilla ./satc.R wtrbuck_prophet10 ${OUT_FOLD}/wtrbuck_prophet10.txt 
+${OUT_FOLD} &
+  nice Rscript --vanilla ./satc.R wtrbuck_prophet25 ${OUT_FOLD}/wtrbuck_prophet25.txt 
+${OUT_FOLD} &
+  nice Rscript --vanilla ./satc.R wtrbuck_prophet50 ${OUT_FOLD}/wtrbuck_prophet50.txt 
+${OUT_FOLD} &
+  nice Rscript --vanilla ./satc.R wtrbuck_prophet75 ${OUT_FOLD}/wtrbuck_prophet75.txt 
+${OUT_FOLD} &
+  nice Rscript --vanilla ./satc.R wtrbuck_prophom10 ${OUT_FOLD}/wtrbuck_prophom10.txt 
+${OUT_FOLD} &
+  nice Rscript --vanilla ./satc.R wtrbuck_prophom25 ${OUT_FOLD}/wtrbuck_prophom25.txt 
+${OUT_FOLD} &
+  nice Rscript --vanilla ./satc.R wtrbuck_prophom50 ${OUT_FOLD}/wtrbuck_prophom50.txt 
+${OUT_FOLD} &
+  nice Rscript --vanilla ./satc.R wtrbuck_prophom75 ${OUT_FOLD}/wtrbuck_prophom75.txt 
+${OUT_FOLD} &
+    
+    nice Rscript --vanilla ./satc.R whales_prophet10 ${OUT_FOLD}/whales_prophet10.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R whales_prophet25 ${OUT_FOLD}/whales_prophet25.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R whales_prophet50 ${OUT_FOLD}/whales_prophet50.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R whales_prophet75 ${OUT_FOLD}/whales_prophet75.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R whales_prophom10 ${OUT_FOLD}/whales_prophom10.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R whales_prophom25 ${OUT_FOLD}/whales_prophom25.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R whales_prophom50 ${OUT_FOLD}/whales_prophom50.txt 
+${OUT_FOLD} &
+    nice Rscript --vanilla ./satc.R whales_prophom75 ${OUT_FOLD}/whales_prophom75.txt 
+${OUT_FOLD} &
+    
