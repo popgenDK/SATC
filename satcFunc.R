a2a38c0594f41762fcdae7a6b9dc390348bce15b
Anders Albrechtsen <andersdetermig@gmail.com>
2021-07-05 17:04:32 +0200

    Update satcFunc.R
    removed the scan function from inside the function.

--- satcFunc.R
+++ satcFunc.R
@@ -72,8 +72,8 @@ filterScaffold <- function(dat,minLength=1e5,M=5,normScaffolds = NULL,range=c(0.
 
     ## scaffolds use for normalization
     if(!is.null(normScaffolds)){
-        normScaffolds <- scan(normScaffolds, what="d")
-        if(!all(normScaffolds%in%filtered[[1]]$scaffolds))
+      #  normScaffolds <- scan(normScaffolds, what="d")
+        if(!all(normScaffolds%in%filtered[[1]][,1]))
             warnings("Chosen normalizing scaffold not found (after min length filtering)")
         normScarfs <- filtered[[1]]$scaffold%in%normScaffolds
     }
